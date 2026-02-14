fd56af23676e14822c449614c5872431621dce25
TLouison <toddmlouison@gmail.com>
2026-02-14 16:07:02 -0500

    Force LINKER_LANGUAGE

--- http-led/src/Server/Server.h
+++ http-led/src/Server/Server.h
@@ -1,6 +1,8 @@
 #ifndef SERVER_H
 #define SERVER_H
 
+#include <Gpio.h>
+
 #include <thread>
 #include <httplib.h>
 #include <nlohmann/json.hpp>
@@ -12,32 +14,32 @@ namespace Server
     class GpioServer
     {
     public:
-        GpioServer(Gpio::GpioManager manager)
+        GpioServer(Gpio::Manager manager)
         {
             // Set up routes
-            svr.Get("/led/status", [&manager](const httplib::Request &, httplib::Response &res)
+            svr.Get("/led/status", [this, &manager](const httplib::Request &, httplib::Response &res)
                     { 
                         Gpio::Pin* led = manager.getPinWithName("led");
-                        res.set_content(get_level_response(led.get()).dump(), "application/json"); });
-            svr.Post("/led/on", [&manager](const httplib::Request &, httplib::Response &res)
+                        res.set_content(this->get_level_response(led).dump(), "application/json"); });
+            svr.Post("/led/on", [this, &manager](const httplib::Request &, httplib::Response &res)
                      { 
                         Gpio::Pin* led = manager.getPinWithName("led");
                         led->set_high();
-                        res.set_content(get_generic_success().dump(), "application/json"); });
-            svr.Post("/led/off", [&manager](const httplib::Request &, httplib::Response &res)
+                        res.set_content(this->get_generic_success().dump(), "application/json"); });
+            svr.Post("/led/off", [this, &manager](const httplib::Request &, httplib::Response &res)
                      { 
                         Gpio::Pin* led = manager.getPinWithName("led");
                         led->set_low();
-                        res.set_content(get_generic_success().dump(), "application/json"); });
-            svr.Post("/stop", [&manager](const httplib::Request &, httplib::Response &res)
-                     { svr.stop(); });
+                        res.set_content(this->get_generic_success().dump(), "application/json"); });
+            svr.Post("/stop", [this, &manager](const httplib::Request &, httplib::Response &res)
+                     { this->svr.stop(); });
 
             // Run the server in a separate thread
             std::thread svr_t([&]()
                               {
                 std::cout << "Server listening on http://localhost:8080" << std::endl;
                 // This call is blocking
-                if (!this.svr.listen("0.0.0.0", 8080))
+                if (!this->svr.listen("0.0.0.0", 8080))
                 {
                     std::cerr << "Server stopped in error state" << std::endl;
                 } });
@@ -54,7 +56,7 @@ namespace Server
             return {{"success", true}};
         }
 
-        json get_level_response(Pin *led)
+        json get_level_response(Gpio::Pin *led)
         {
             return {{"level", led->get_level()}, {"success", true}};
         }
